0c764caa9720a140bbb2094da296cb5c3e203cef
Fiona1777 <fshavilof@csustan.edu>
2025-05-07 10:00:33 -0700

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -83,10 +83,10 @@ const CONFIG = {
       '', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
-    'Genome Assembly',
+    'Gel Electrophoresis',
     'DNA/RNA Extraction',
-    'JavaScript',
-    'React.js',
+    'Microbiology',
+    'Bioinformatics',
   ],
   experiences: [
     {
@@ -126,10 +126,10 @@ const CONFIG = {
       to: '2022',
     },
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2012',
-      to: '2014',
+      institution: 'CSU Stanislaus',
+      degree: 'B.S. in Biological Sciences concentration in Molecular,Cellular, and Microbial Organisms',
+      from: '2022',
+      to: '2025',
     },
   ],
   publications: [
@@ -143,13 +143,13 @@ const CONFIG = {
         'Here we present the draft genome sequence of Intestinibacter bartletti DSM 16795.',
     },
     {
-      title: 'Publication Title',
-      conferenceName: 'Conference Name',
+      title: '',
+      conferenceName: '',
       journalName: '',
-      authors: 'John Doe, Jane Smith',
-      link: 'https://example.com',
+      authors: '',
+      link: '',
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        '',
     },
   ],
   // Display articles from your medium or dev account. (Optional)
